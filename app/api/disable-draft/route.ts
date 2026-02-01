b40b54e09fb2c112dc735afc31da11d87c359707
Fotober RnD <rnd@fotober.com>
2026-02-01 20:21:21 +0700

    Remove Sanity API routes to fix Vercel build

--- app/api/disable-draft/route.ts
+++
@@ -1,8 +0,0 @@
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-export async function GET(request: Request) {
-    ; (await draftMode()).disable()
-    const url = new URL(request.url)
-    redirect(url.searchParams.get('redirect') || '/')
-}
